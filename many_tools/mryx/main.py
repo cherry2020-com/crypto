d2e6e8ff692200e9253d1a19d35114372b7abfe2
Minglei Weng <minglei.weng@veeva.com>
2018-06-20 10:32:51 +0800

    ...

--- many_tools/mryx/main.py
+++ many_tools/mryx/main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 # - * - encoding: UTF-8 - * -
+import random
+import time
+
 from utils.fiddler import RawToPython
 
-raw = RawToPython('./ye_head.txt')
+raw = RawToPython('./ye_head_for_me.txt')
+count = 0
 with open('codes.txt') as cs:
     for line in cs:
+        count += 1
+        if count == 11:
+            break
         code = line.strip()
         if not code:
             continue
         raw.set_param(url_param={'inviteCode': code})
         wb_data = raw.requests()
-        print wb_data.json()['msg']
+        time.sleep(random.randint(1, 5))
+        print code, wb_data.json()['msg']
