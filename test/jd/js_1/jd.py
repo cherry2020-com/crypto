da986bad8e6240176349dd37262dc21734e0d817
minglei.weng <645008699@qq.com>
2018-08-15 22:37:26 +0800

    ff

--- test/jd/js_1/jd.py
+++ test/jd/js_1/jd.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 # - * - encoding: UTF-8 - * -
+import time
+
 from utils.buying_times import PanicBuyingTimes
 from utils.fiddler import RawToPython, FiddlerRequestException
 
 req = RawToPython('./head.txt')
 buying_time = PanicBuyingTimes("2018-08-15 10:00:00")
 while True:
-    try:
-        if buying_time.is_start:
+    if buying_time.is_start:
+        try:
             web_data = req.requests(timeout=(None, 0.01))
             # web_data = req.requests(timeout=(None, None))
             print web_data.json()['subCodeMsg']
-    except FiddlerRequestException:
-        pass
+        except FiddlerRequestException:
+            pass
+    else:
+        time.sleep(0.1)
