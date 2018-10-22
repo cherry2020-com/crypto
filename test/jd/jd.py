134723ee3ffdbd81a6499537e5064cfb6eb78579
minglei.weng <645008699@qq.com>
2018-10-22 23:29:27 +0800

    ff

--- test/jd/jd.py
+++ test/jd/jd.py
@@ -1,53 +1,34 @@
 #!/usr/bin/python
 # - * - encoding: UTF-8 - * -
 import random
-import threading
 import time
 
 import sys
-import uuid
 
-from utils.buying_times import PanicBuyingTimes, PanicBuyingTimesException
+from utils.buying_times import PanicBuyingTimes
 from utils.fiddler import RawToPython, FiddlerRequestException
 
-
-def request_jd(req_jd):
-    try:
-        print '-->:', threading.current_thread().name
-        web_data = req_jd.requests(timeout=1)
-        print web_data.json()['subCodeMsg']
-    except FiddlerRequestException:
-        pass
-
-
-if __name__ == '__main__':
-    file_path = sys.argv[1]
-    date_times = sys.argv[2] if len(sys.argv) == 3 else None
-    date_times = "2018-08-21 18:00:00"
-    buying_time = PanicBuyingTimes(date_times)
-    req = RawToPython(file_path)
-    count = 0
-    heart_count = 1
-    threadings = []
-    while True:
+file_path = sys.argv[1]
+req = RawToPython(file_path)
+buying_time = PanicBuyingTimes("2018-08-20 18:00:00")
+count = 0
+heart_count = 1
+while True:
+    if buying_time.start(debug=True, other_info='{}/{}'.format(count + 1, heart_count)):
         try:
-            if buying_time.start(debug=True,
-                                 other_info='{}/{}'.format(count + 1, heart_count)):
-                t = threading.Thread(target=request_jd, name=uuid.uuid4(), args=(req, ))
-                t.start()
-                time.sleep(0.11)
-                threadings.append(t)
-            else:
-                count += 1
-                if count == heart_count:
-                    heart_count = random.randint(300, 600)
-                    count = 0
-                    try:
-                        web_data = req.requests(timeout=5)
-                        print web_data.json()['subCodeMsg']
-                    except FiddlerRequestException:
-                        pass
-                time.sleep(1)
-        except PanicBuyingTimesException as e:
-            for t in threadings:
-                t.join(timeout=2)
+            web_data = req.requests(timeout=(None, 0.001))
+            # web_data = req.requests(timeout=(None, None))
+            print web_data.json()['subCodeMsg']
+        except FiddlerRequestException:
+            pass
+    else:
+        count += 1
+        if count == heart_count:
+            heart_count = random.randint(300, 600)
+            count = 0
+            try:
+                web_data = req.requests(timeout=10)
+                print web_data.json()['subCodeMsg']
+            except FiddlerRequestException:
+                pass
+        time.sleep(1)
---
+++ test/jd/jd_threading.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# - * - encoding: UTF-8 - * -
+import random
+import threading
+import time
+
+import sys
+import uuid
+
+from utils.buying_times import PanicBuyingTimes, PanicBuyingTimesException
+from utils.fiddler import RawToPython, FiddlerRequestException
+
+
+def request_jd(req_jd):
+    try:
+        print '-->:', threading.current_thread().name
+        web_data = req_jd.requests(timeout=1)
+        print web_data.json()['subCodeMsg']
+    except FiddlerRequestException:
+        pass
+
+
+if __name__ == '__main__':
+    file_path = sys.argv[1]
+    date_times = "2018-10-22 00:00:00"
+    date_times = sys.argv[2] if len(sys.argv) == 3 else date_times
+    buying_time = PanicBuyingTimes(date_times)
+    req = RawToPython(file_path)
+    count = 0
+    heart_count = 1
+    threadings = []
+    while True:
+        try:
+            if buying_time.start(debug=True,
+                                 other_info='{}/{}'.format(count + 1, heart_count)):
+                t = threading.Thread(target=request_jd, name=uuid.uuid4(), args=(req, ))
+                t.start()
+                time.sleep(0.3)
+                threadings.append(t)
+            else:
+                count += 1
+                if count == heart_count:
+                    heart_count = random.randint(300, 600)
+                    count = 0
+                    try:
+                        web_data = req.requests(timeout=5)
+                        print web_data.json()['subCodeMsg']
+                    except FiddlerRequestException:
+                        pass
+                time.sleep(1)
+        except PanicBuyingTimesException as e:
+            for t in threadings:
+                t.join(timeout=2)
