9e9c212f01522cc850b4ac119b38d7caf46f09c8
minglei.weng <645008699@qq.com>
2018-12-29 20:53:27 +0800

    ff

--- test/360bug_ok/main.py
+++ test/360bug_ok/main.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import random
 import re
 import time
 
 from bs4 import BeautifulSoup
 
+from utils.buying_times import PanicBuyingTimes
 from utils.fiddler_session import RawToPython
 
 
 DEBUG = False
+USER = '176****5665'
 
 
 def get_product_id(entrance_text):
     if DEBUG:
         with open('./head/test.txt') as f:
             entrance_text = f.read()
+    if USER not in entrance_text:
+        print u'--> 登陆失效: {}'.format(USER)
+        raise Exception()
     bs = BeautifulSoup(entrance_text, "lxml")
     url = bs.find('div', class_='mainbody').find(
         'div', class_='flash-product-list').a.attrs.get('href')
-    print u'--> 得到抢购链接: {}'.format(url)
     if url == '#':
+        print u'--> 抢购链接未开放: {}'.format(url)
         return None
+    print u'--> 得到抢购链接: {}'.format(url)
     code = re.findall(r'\d{5,20}', url)[0]
     print u'--> 得到产品ID: {}'.format(code)
     return str(code)
@@ -37,34 +44,35 @@ def get_postkey(postkey_text):
 
 if __name__ == '__main__':
     entrance = RawToPython('./head/get_entrance.txt')
+    time_is = PanicBuyingTimes(['2018-12-29 22:00:00'])
     while True:
         entrance_request = entrance.requests()
         print u'--> 入口请求状态码: {}'.format(entrance_request.status_code)
         if entrance_request.status_code == 200:
             product_id = get_product_id(entrance_request.text)
-            if product_id is None:
-                continue
-            with open('./head/get_postkey.txt') as f:
-                postkey_raw = f.read().format(product_id=product_id)
-            postkey = RawToPython(None, postkey_raw)
-            postkey.session.cookies = entrance.session.cookies
-            postkey_request = postkey.requests()
-            print u'--> postkey请求状态码: {}'.format(postkey_request.status_code)
-            if postkey_request.status_code == 200:
-                post_key, yj_id = get_postkey(postkey_request.text)
+            if product_id is not None:
+                with open('./head/get_postkey.txt') as f:
+                    postkey_raw = f.read().format(product_id=product_id)
+                postkey = RawToPython(None, postkey_raw)
+                postkey.session.cookies = entrance.session.cookies
+                postkey_request = postkey.requests()
+                print u'--> postkey请求状态码: {}'.format(postkey_request.status_code)
+                if postkey_request.status_code == 200:
+                    post_key, yj_id = get_postkey(postkey_request.text)
 
-                with open('./head/sub_order.txt') as f:
-                    sub_order_raw = f.read().format(product_id=product_id,
-                                                    postkey=post_key)
-                sub_order = RawToPython(None, sub_order_raw)
-                sub_order.session.cookies = postkey.session.cookies
-                if yj_id:
-                    sub_order.set_param(body_param={'yj_id': yj_id})
-                sub_order_request = sub_order.requests()
-                print u'--> 抢购结果: {}'.format(sub_order_request.text)
-                break
-        if DEBUG:
-            sleep_time = 1
-        else:
+                    with open('./head/sub_order.txt') as f:
+                        sub_order_raw = f.read().format(product_id=product_id,
+                                                        postkey=post_key)
+                    sub_order = RawToPython(None, sub_order_raw)
+                    sub_order.session.cookies = postkey.session.cookies
+                    if yj_id:
+                        sub_order.set_param(body_param={'yj_id': yj_id})
+                    sub_order_request = sub_order.requests()
+                    print u'--> 抢购结果: {}'.format(sub_order_request.text)
+                    break
+        if time_is.is_start:
             sleep_time = 0.01
+        else:
+            sleep_time = random.randint(5, 20)
+        print u'--> 延时: {}s'.format(sleep_time)
         time.sleep(sleep_time)
--- utils/buying_times.py
+++ utils/buying_times.py
@@ -10,9 +10,10 @@ class PanicBuyingTimesException(Exception):
 
 
 class PanicBuyingTimes(object):
-    def __init__(self, date_times, before_seconds=3, after_seconds=5):
+    def __init__(self, date_times, before_seconds=3, after_seconds=5, debug_info=True):
         self.before_seconds = before_seconds
         self.after_seconds = after_seconds
+        self.debug_info = debug_info
         self.date_times_queue = Queue.Queue()
         if isinstance(date_times, str):
             date_times = [date_times]
@@ -62,22 +63,22 @@ class PanicBuyingTimes(object):
     def is_start(self):
         return self.start()
 
-    def start(self, debug=False, other_info=''):
+    def start(self, other_info=''):
         now = datetime.datetime.now()
         start_time, end_time = self.this_time
         if start_time <= now <= end_time:
-            if debug:
-                print "{} - True - {} - {}".format(start_time, now.strftime("%Y-%m-%d %H:%M:%S"),
-                                              other_info)
+            if self.debug_info:
+                print "IS_START: True [{}] == [{}] ({})".format(
+                    start_time, now.strftime("%Y-%m-%d %H:%M:%S"), other_info)
             return True
         if now >= end_time:
             try:
                 self.this_time = self.date_times_queue.get(block=False)
             except Queue.Empty:
                 raise PanicBuyingTimesException('Error: Had not times to wait !')
-        if debug:
-            print "{} - False - {} - {}".format(start_time, now.strftime("%Y-%m-%d %H:%M:%S"),
-                                           other_info)
+        if self.debug_info:
+            print "IS_START: False [{}] == [{}] ({})".format(
+                start_time, now.strftime("%Y-%m-%d %H:%M:%S"), other_info)
         return False
 
 
