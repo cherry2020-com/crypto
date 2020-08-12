a91179e7275dfeb3cd14a3ce0e34e1dffea0f084
Minglei Weng <minglei.weng@veeva.com>
2020-08-12 21:40:48 +0800

    ...

--- test/workinwork/tt.py
+++ test/workinwork/tt.py
@@ -8,8 +8,8 @@ from bs4 import BeautifulSoup
 from utils.fiddler import RawToPython
 
 PLANNED_START_CITY_NAME = 'ALBION PARK'
-UNPLANNED_START_CITY_NAME = ''
-START_ID = 1
+UNPLANNED_START_CITY_NAME = 'CASTLEREAGH'
+START_ID = 3
 
 
 def get_all_citys():
@@ -34,7 +34,8 @@ def get_plan_info(city_name):
     last_update_on = soups.find_all(id="form1:tabSet1:tab1:lastUpdatedTime")
     if last_update_on:
         last_update_on = last_update_on[0].text
-        print '-- >last_update_on:', last_update_on
+        last_update_on_str = 'last_update_on: ' + last_update_on
+        print '-- >', last_update_on_str
     else:
         last_update_on = ''
     datails_str = soups.find_all(
@@ -61,7 +62,8 @@ def get_plan_info(city_name):
                 all_details.append(dict(zip(excel_titles, line_data)))
             if all_details:
                 sql1 = get_insert_sql1(
-                    post_code='', city=city_name, interruptions=last_update_on,
+                    post_code='', city=city_name,
+                    interruptions=last_update_on_str,
                     type='planned')
                 print sql1
                 for each in all_details:
@@ -91,25 +93,24 @@ def get_unplan_info(city_name):
     total_customers_affected = soups.find_all(id="form1:tabSet1:tab2:totalCustomersAffected")
     if total_customers_affected:
         total_customers_affected = total_customers_affected[0].text
-        print '-->total_customers_affected:'
-        print '\t', total_customers_affected
+        total_customers_affected_str = 'total_customers_affected: ' + total_customers_affected
+        print '-- >', total_customers_affected_str
     empty_datails_str = soups.find_all(
         id="form1:tabSet1:tab2:unplannedOutagesTable:tableRowGroupUnPlanned:_emptyDataColumn:_emptyDataText")
     if empty_datails_str:
         datails_str = empty_datails_str[0]
-        print '--> detail:'
-        print datails_str.text
+        print '-- > detail:', datails_str.text
     else:
-        raise Exception('GET UNPLAIN DATA')
-        datails_soup = soups.find_all(id="form1:tabSet1:tab1:plannedOutagesTable")
+        datails_soup = soups.find_all(id="form1:tabSet1:tab2:unplannedOutagesTable")
         if datails_soup:
             datails_soup = datails_soup[0]
             excel_titles = datails_soup.find_all(
-                id='form1:tabSet1:tab1:plannedOutagesTable:tableRowGroupPlanned:_columnHeaderBar:0')[
+                id='form1:tabSet1:tab2:unplannedOutagesTable:tableRowGroupUnPlanned:_columnHeaderBar:0')[
                 0]
             excel_titles = [x.strip() for x in excel_titles.text.split('\n') if
                             x.strip()]
-            line_templ = "form1:tabSet1:tab1:plannedOutagesTable:tableRowGroupPlanned:{}"
+            excel_titles = ["{} {}".format(excel_titles[0], excel_titles[1])] + excel_titles[2:]
+            line_templ = "form1:tabSet1:tab2:unplannedOutagesTable:tableRowGroupUnPlanned:{}"
             all_details = []
             for i in range(99999):
                 line_id = line_templ.format(i)
@@ -119,8 +120,26 @@ def get_unplan_info(city_name):
                 line_data = line_data[0]
                 line_data = [x for x in line_data.text.split('\n') if x.strip()]
                 all_details.append(dict(zip(excel_titles, line_data)))
-            print '--> detail:'
-            pprint(all_details)
+            if all_details:
+                sql1 = get_insert_sql1(
+                    post_code='', city=city_name,
+                    interruptions=total_customers_affected_str,
+                    type='unplanned')
+                print sql1
+                for each in all_details:
+                    sql1 = get_insert_sql2(
+                        post_code=each['Post Code'],
+                        city=each['Suburb'],
+                        street=each['Street'],
+                        start_time='',
+                        finish_time='',
+                        cause=each['Cause'], status=each['Status'],
+                        estimated_restoration_time=each['Estimated Restoration Time'],
+                        number_of_customers_affected=each['Number of Customers Affected'],
+                    )
+                    print sql1
+                print '--', '-' * 50
+            print '-- > detail:', all_details
 
 
 def get_insert_sql1(**kwargs):
@@ -144,13 +163,13 @@ VALUES
 
 if __name__ == '__main__':
     all_citys = get_all_citys()
-    planned_citys = all_citys
-    if PLANNED_START_CITY_NAME:
-        planned_citys = all_citys[all_citys.index(PLANNED_START_CITY_NAME):]
-    for planned_city_name in planned_citys:
-        get_plan_info(planned_city_name)
-    # unplanned_citys = all_citys
-    # if UNPLANNED_START_CITY_NAME:
-    #     unplanned_citys = all_citys[all_citys.index(PLANNED_START_CITY_NAME):]
-    # for unplanned_city_name in planned_citys:
-    #     get_unplan_info(unplanned_city_name)
+    # planned_citys = all_citys
+    # if PLANNED_START_CITY_NAME:
+    #     planned_citys = all_citys[all_citys.index(PLANNED_START_CITY_NAME):]
+    # for planned_city_name in planned_citys:
+    #     get_plan_info(planned_city_name)
+    unplanned_citys = all_citys
+    if UNPLANNED_START_CITY_NAME:
+        unplanned_citys = all_citys[all_citys.index(UNPLANNED_START_CITY_NAME):]
+    for unplanned_city_name in unplanned_citys:
+        get_unplan_info(unplanned_city_name)
