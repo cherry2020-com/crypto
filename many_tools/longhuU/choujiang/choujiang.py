4071862c47935ffa00d07c48b7e40ed76d128653
Minglei Weng <minglei.weng@veeva.com>
2020-12-17 14:29:14 +0800

    1234

--- many_tools/longhuU/choujiang/choujiang.py
+++ many_tools/longhuU/choujiang/choujiang.py
@@ -10,12 +10,16 @@ imp_templ = u'\033[1;33;44m{}\033[0m'
 
 if __name__ == '__main__':
    rtp = RawToPython('./13511004353.txt')
-   print rtp.requests().text
+   print '13511004353', rtp.requests().text
    rtp = RawToPython('./15578801243.txt')
-   print rtp.requests().text
+   print '15578801243', rtp.requests().text
    rtp = RawToPython('./15578872074.txt')
-   print rtp.requests().text
+   print '15578872074', rtp.requests().text
    rtp = RawToPython('./17600196974.txt')
-   print rtp.requests().text
+   print '17600196974', rtp.requests().text
    rtp = RawToPython('./18842670608.txt')
-   print rtp.requests().text
+   print '18842670608', rtp.requests().text
+   rtp = RawToPython('./17601605665.txt')
+   print '17601605665', rtp.requests().text
+   rtp = RawToPython('./18841164712.txt')
+   print '18841164712', rtp.requests().text
