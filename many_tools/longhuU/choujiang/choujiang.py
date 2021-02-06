ddb993610e8f284a735d41e571d9730e21c0d52f
Minglei Weng <minglei.weng@veeva.com>
2021-02-06 23:01:33 +0800

    ...

--- many_tools/longhuU/choujiang/choujiang.py
+++ many_tools/longhuU/choujiang/choujiang.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # - * - encoding: UTF-8 - * -
+import os
 import sys
 from datetime import datetime
 sys.path.extend(['/data/coding/my_tools/'])
@@ -14,19 +15,20 @@ def rrr(file_name):
    rtp.set_param(req_param={'combinedId': '1987',
                             'componentId': 'mod_1611911779711',
                             'wheelId': '9718654f621811ebb7c87cd30adaf108'})
-   print file_name, rtp.requests().text
+   print os.path.basename(file_name), rtp.requests().text
 
 
 if __name__ == '__main__':
+   parent_path = os.path.dirname(os.path.abspath(__file__))
    file_names = [
-      './13511004353.txt',
-      './15578801243.txt',
-      './15578872074.txt',
-      './17600196974.txt',
-      # './18842670608.txt',
-      './17601605665.txt',
-      './18841164712.txt',
-      './15369204212.txt',
+      '13511004353.txt',
+      '15578801243.txt',
+      '15578872074.txt',
+      '17600196974.txt',
+      # '18842670608.txt',
+      '17601605665.txt',
+      '18841164712.txt',
+      '15369204212.txt',
    ]
    for file_name in file_names:
-      rrr(file_name)
+      rrr(os.path.join(parent_path, file_name))
