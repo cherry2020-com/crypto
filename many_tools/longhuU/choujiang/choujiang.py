f4686e68c8df7a217afcdf0be9b1bd7edb6db576
Minglei Weng <minglei.weng@veeva.com>
2021-02-06 23:06:51 +0800

    ...

--- many_tools/longhuU/choujiang/choujiang.py
+++ many_tools/longhuU/choujiang/choujiang.py
@@ -15,7 +15,7 @@ def rrr(file_name):
    rtp.set_param(req_param={'combinedId': '1987',
                             'componentId': 'mod_1611911779711',
                             'wheelId': '9718654f621811ebb7c87cd30adaf108'})
-   print os.path.basename(file_name), rtp.requests().text
+   print datetime.now().strftime('%Y-%m-%d %H:%M:%S'), os.path.basename(file_name), rtp.requests().text
 
 
 if __name__ == '__main__':
