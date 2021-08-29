1590b63ea2d663f773038637c4d72797b675de78
minglei.weng <minglei.weng>
2021-08-29 17:16:29 +0800

    ...

--- small_tools/hospital_schedule/__init__.py
+++ small_tools/hospital_schedule/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 IS_DEBUG_SUBMIT = False
-IS_DEBUG_TIME = True
+IS_DEBUG_TIME = False
 FILE_PATH = './fuchan_head'
 
 SLOW_TIMEOUT = 10
-FAST_TIMEOUT = 1
-HARD_TIMEOUT = 1
-ERROR_COUNT = 5
+FAST_TIMEOUT = 0.1
+HARD_TIMEOUT = 0.1
+ERROR_COUNT = 10
 APP_ACCESS_TOKEN = 'UGlpFAlTvmwL9XWK4x2GLpjqxC8m2yyljYfCCVYaF4w'
 HAS_CARD = True
 
@@ -17,5 +17,5 @@ HOSPITAL = u'总院'
 # DOCTOR = u'战芳'
 DEPARTMENT = u'产一门诊'
 DOCTOR = u'李颖'
-DATE = u'2021-09-03'
-TIME = u'09:42'
+DATE = u'2021-09-10'
+TIME = u'08:00'
